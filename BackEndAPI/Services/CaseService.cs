15bdb0a6e9782187e0a53f4429b229c19895172f
fatma-sabry <fsabre118@gmail.com>
2023-02-28 00:20:27 +0200

    Add IGenericService, Add Services Of Models

---
+++ BackEndAPI/Services/CaseService.cs
@@ -0,0 +1,34 @@
+﻿using BackEndAPI.Interfaces;
+using BackEndAPI.Models;
+using System.Collections.Generic;
+
+namespace BackEndAPI.Services
+{
+    public class CaseService : IGenericService<Case>
+    {
+        public void Create(Case model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Case model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Case> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Case GetByID(int ID)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Case model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
