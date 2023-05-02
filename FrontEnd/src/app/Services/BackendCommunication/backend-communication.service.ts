1b2a0574698257cde6e04723ccc8a5fcb2f58f53
SteveLorde <mostafa.maher98@gmail.com>
2023-05-02 02:35:01 +0200

    tidy up endpoints in frontend

--- FrontEnd/src/app/Services/BackendCommunication/backend-communication.service.ts
+++ FrontEnd/src/app/Services/BackendCommunication/backend-communication.service.ts
@@ -5,6 +5,7 @@ import { Observable} from 'rxjs';
 import { CaseDTO } from 'src/app/Models/CaseDTO'
 import { Charity } from 'src/app/Models/CharityDTO'
 import { UserDTO } from '../../Models/UserDTO';
+import { CasePayment } from '../../Models/CasePayment';
 
 @Injectable({
   providedIn: 'root'
@@ -19,72 +20,23 @@ export class BackendCommunicationService {
   //PROJECT Endpoints
   //---------------------------------
 
-
-  //GET
-  //----------------------------------------
-  //GET HTTP for Cases
+  //Case
   getCases(): Observable<CaseDTO> {
     return this.http.get<CaseDTO>(this.serverUrl + 'api/Cases');
   }
 
-  //GET HTTP For Case by ID
   getCasesById(id: any): Observable<CaseDTO> {
     return this.http.get<CaseDTO>(this.serverUrl + `api/Cases/(getCase/${id}`);
   }
 
-
-  //GET HTTP for Charities
-  getCharity(): Observable<Charity> {
-    return this.http.get<Charity>(this.serverUrl + 'api/Charity');
-  }
-
-  //GET HTTP for Users by ID
-  getCharitybyId(id: any): Observable<Charity> {
-    return this.http.get<Charity>(this.serverUrl + `api/Charity/getCharity/${id}`);
-  }
-
-
-  //GET HTTP for Users
-  getUsers(): Observable<UserDTO[]> {
-    return this.http.get<UserDTO[]>(this.serverUrl + 'api/User');
-  }
-
-  //GET HTTP for Users by ID
-  getUserbyId(id: any): Observable<UserDTO> {
-    return this.http.get<UserDTO>(this.serverUrl + `api/User/(getUser/${id}`);
-  }
-
-  //----------------------------------
-
-  //-POST
-  //-----------------------------
-  //POST HTTP for Adding Case
   addCase(troubled: any): Observable<any> {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
     return this.http.post<any>(this.serverUrl + 'api/Cases/AddNewCase', troubled, httpOptions);
   }
 
-  //POST HTTP for Adding Charity
-  addCharity(charity: any): Observable<any> {
-    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-    return this.http.post<any>(this.serverUrl + 'api/Charity/AddNewCharity', charity, httpOptions);
-  }
-
-  //-----------------------------------------------
-
-
-  //PATCH HTTP for Updating a Case
   updateCase(troubled: any, id: any): Observable<any> {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
     return this.http.put<any>(this.serverUrl + `api/Cases/updateCase/${id}`, troubled, httpOptions);
-  }
-
-  //-----------------------------------------
-
-
-  //DELETE HTTP
-  public delete(url: string, options?: any) {
-    return this.http.delete(this.serverUrl, options);
   }
 
   DeleteCase(id: any): Observable<any> {
@@ -92,9 +44,88 @@ export class BackendCommunicationService {
     return this.http.delete<any>(this.serverUrl + `api/Case/deleteCase/${id}`);
   }
 
+
+  //Charity
+  getCharity(): Observable<Charity> {
+    return this.http.get<Charity>(this.serverUrl + 'api/Charity');
+  }
+
+  getCharitybyId(id: any): Observable<Charity> {
+    return this.http.get<Charity>(this.serverUrl + `api/Charity/getCharity/${id}`);
+  }
+
+  addCharity(charity: any): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<any>(this.serverUrl + 'api/Charity/AddNewCharity', charity, httpOptions);
+  }
+
+  UpdateCharityby(id: any): Observable<Charity> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
+    return this.http.put<Charity>(this.serverUrl + `api/Charity/updateCharity/${id}`, httpOptions);
+  }
+
   DeleteCharity(id: any): Observable<any> {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
     return this.http.delete<any>(this.serverUrl + `api/Charity/deleteCharity/${id}`);
+  }
+
+
+  //User
+  getUsers(): Observable<UserDTO> {
+    return this.http.get<UserDTO>(this.serverUrl + 'api/User');
+  }
+
+  getUserbyId(id: any): Observable<UserDTO> {
+    return this.http.get<UserDTO>(this.serverUrl + `api/User/(getUser/${id}`)
+  }
+
+  addUser(user: any): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<any>(this.serverUrl + '/api/User/AddNewUser', user, httpOptions);
+  }
+
+  UpdateUser(user: any, id: any): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.put<any>(this.serverUrl + `/api/User/updateUser/${id}`, user, httpOptions);
+  }
+
+  DeleteUser(id: any): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.delete<any>(this.serverUrl + `api/Case/deleteCase/${id}`);
+  }
+
+
+
+
+
+  //CasePayment
+  getCasePayment(): Observable<CasePayment> {
+    return this.http.get<CasePayment>(this.serverUrl + 'api/CasePayment');
+  }
+
+  getCasePaymentByID(id: any): Observable<CasePayment> {
+    return this.http.get<CasePayment>(this.serverUrl + `/api/CasePayment/(getCasePayment/${id}`);
+  }
+
+  AddCasePayment(): Observable<CasePayment> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
+    return this.http.post<CasePayment>(this.serverUrl + 'api/CasePayment/AddNewCasePayment', httpOptions)
+  }
+
+  UpdateCasePaymentBy(id: any): Observable<CasePayment> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
+    return this.http.put<CasePayment>(this.serverUrl + `/api/CasePayment/updateCasePayment/${id}`, httpOptions)
+  }
+
+  DeleteCasePayment(): Observable<CasePayment> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
+    return this.http.delete<CasePayment>(this.serverUrl + 'api/CasePayment/AddNewCasePayment', httpOptions)
+  }
+
+
+  //DELETE HTTP
+  public delete(url: string, options?: any) {
+    return this.http.delete(this.serverUrl, options);
   }
 
 }
