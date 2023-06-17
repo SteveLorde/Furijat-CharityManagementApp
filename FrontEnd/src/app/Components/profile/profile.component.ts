bd9313b4fd263219537056d9ca32100ad2f637ee
HAJAR <hagermohamed1112255@gmail.com>
2023-06-17 12:49:48 +0300

    before merging database fix

--- FrontEnd/src/app/Components/profile/profile.component.ts
+++ FrontEnd/src/app/Components/profile/profile.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDTO } from '../../Models/UserDTO';
-import { UserType } from 'src/app/Models/UserType'
-import { AuthService } from 'src/app/Services/Authorization/auth.service'
+import { UserType } from 'src/app/Models/UserType';
+import { AuthService } from 'src/app/Services/Authorization/auth.service';
 import { BackendCommunicationService } from '../../Services/BackendCommunication/backend-communication.service';
 
+export interface PagingConfig {
+  currentPage: number;
+  itemsPerPage: number;
+  totalItems: number;
+}
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
   //create object "user" of User model
-  user = {} as UserDTO
-  id: any
-
-  constructor(private _servercom: BackendCommunicationService) { }
+  user = {} as UserDTO;
+  id: any;
+  p: number = 1;
+  constructor(private _servercom: BackendCommunicationService) {}
 
   ngOnInit(): void {
-    this.id = localStorage.getItem('UID')
-    this.GetProfile()
+    this.id = localStorage.getItem('UID');
+    this.GetProfile();
   }
 
   GetProfile() {
     this._servercom.getUserbyId(this.id).subscribe((res: any) => {
-      this.user = res
-    })
+      this.user = res;
+    });
   }
 }
--- FrontEnd/src/app/Components/userlog/userlog.component.ts
+++ FrontEnd/src/app/Components/userlog/userlog.component.ts
@@ -5,28 +5,29 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { DonatelogService } from 'src/app/Services/DonateLog/donatelog.service';
 import { Charity } from '../../Models/CharityDTO';
 
-
 @Component({
   selector: 'app-userlog',
   templateUrl: './userlog.component.html',
-  styleUrls: ['./userlog.component.css']
+  styleUrls: ['./userlog.component.css'],
 })
 export class UserlogComponent implements OnInit {
+  DonatedCases: CaseDTO[] = [];
+  DonatedCharities: Charity[] = [];
 
-  DonatedCases: CaseDTO[] = []
-  DonatedCharities: Charity[] = []
-
-  constructor(private donatelog: DonatelogService) { }
+  constructor(private donatelog: DonatelogService) {}
 
   ngOnInit(): void {
-    this.LogDonationCharities()
-    console.log("showing results in donatedcharitieslog" + this.DonatedCharities)
+    this.LogDonationCharities();
+    console.log(
+      'showing results in donatedcharitieslog' + this.DonatedCharities
+    );
   }
 
   LogDonationCharities() {
-    this.DonatedCharities = this.donatelog.ReturnDonatedCharities()
+    this.DonatedCharities = this.donatelog.ReturnDonatedCharities();
   }
-
-
-
+  dateTime: Date;
+  ngoninit() {
+    this.dateTime = new Date();
+  }
 }
