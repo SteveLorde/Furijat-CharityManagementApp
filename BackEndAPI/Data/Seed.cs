2fbf270e4d7e2689afc972acfe666697e57aab6c
m_abdelkawy <mustafa.abdelkawy@vovara.com>
2023-06-17 20:29:28 +0300

    seeding users

---
+++ BackEndAPI/Data/Seed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using BackEndAPI.Database;
+using BackEndAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEndAPI.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUser(FurijatContext context)
+        {
+            if (await context.Users.AnyAsync()) return;
+
+            var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonSerializer.Deserialize<List<User>>(userData);
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+                user.UserName = user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("12345"));
+                user.PasswordSalt = hmac.Key;
+
+                context.Users.Add(user);
+            }
+            await context.SaveChangesAsync();
+        }
+
+    }
+}
