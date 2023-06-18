c14b2ca9534d6ca79f87345b62d76d9440535eb2
m_abdelkawy <mustafa.abdelkawy@vovara.com>
2023-06-18 20:24:02 +0300

    edits

--- BackEndAPI/Data/FurijatContext.cs
+++ BackEndAPI/Data/FurijatContext.cs
@@ -5,13 +5,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BackEndAPI.Database
 {
-    public class FurijatContext:DbContext
+    public class FurijatContext : DbContext
     {
         public FurijatContext(DbContextOptions<FurijatContext> option) : base(option)
         {
-        
+
         }
-    
+
         public DbSet<User> Users { get; set; }
         public DbSet<Creditor> Creditor { get; set; }
         public DbSet<Charity> Charities { get; set; }
@@ -28,8 +28,22 @@ namespace BackEndAPI.Database
             modelBuilder.Entity<Admin>().ToTable("Admin");
             modelBuilder.Entity<Donator>().ToTable("Donators");
             modelBuilder.Entity<Creditor>().ToTable("Creditor").HasKey(x => new { x.CreditorID, x.CaseID });
-            modelBuilder.Entity<Donation>().ToTable("Donation").HasKey(d => new { d.CaseId,d.DonatorId,d.CharityId });
-            base .OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Donation>().ToTable("Donation").HasKey(d => new { d.CaseId, d.DonatorId, d.CharityId });
+            modelBuilder.Entity<Case>()
+            .Property(c => c.CurrentAmount)
+            .HasPrecision(18, 2);
+            modelBuilder.Entity<Case>()
+            .Property(c => c.TotalAmount)
+            .HasPrecision(18, 2);
+            modelBuilder.Entity<Creditor>()
+            .Property(c => c.Deserves_Amount)
+            .HasPrecision(18, 2);
+            modelBuilder.Entity<Donation>()
+            .Property(c => c.Amount)
+            .HasPrecision(18, 2);
+            modelBuilder.Entity<Donator>()
+            .Property(c => c.PaidAmount)
+            .HasPrecision(18, 2);
         }
 
     }
---
+++ BackEndAPI/Migrations/20230618171050_IntialMigration.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEndAPI.Migrations
+{
+    public partial class IntialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropTable(name: "Charities");
+            //migrationBuilder.Sql("DROP DATABASE FurijatDb;");
+            migrationBuilder.CreateTable(
+                name: "Charities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Bank_Account = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Website = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Charities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Admin_Users_Id",
+                        column: x => x.Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    CharityId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrentAmount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MarriageStatus = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cases_Charities_CharityId",
+                        column: x => x.CharityId,
+                        principalTable: "Charities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cases_Users_Id",
+                        column: x => x.Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Donators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaidAmount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donators", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Donators_Users_Id",
+                        column: x => x.Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Creditor",
+                columns: table => new
+                {
+                    CreditorID = table.Column<int>(type: "int", nullable: false),
+                    CaseID = table.Column<int>(type: "int", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Payment_Account = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Deserves_Amount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Creditor", x => new { x.CreditorID, x.CaseID });
+                    table.ForeignKey(
+                        name: "FK_Creditor_Cases_CaseID",
+                        column: x => x.CaseID,
+                        principalTable: "Cases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Donation",
+                columns: table => new
+                {
+                    CaseId = table.Column<int>(type: "int", nullable: false),
+                    CharityId = table.Column<int>(type: "int", nullable: false),
+                    DonatorId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donation", x => new { x.CaseId, x.DonatorId, x.CharityId });
+                    table.ForeignKey(
+                        name: "FK_Donation_Cases_CaseId",
+                        column: x => x.CaseId,
+                        principalTable: "Cases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Donation_Charities_CharityId",
+                        column: x => x.CharityId,
+                        principalTable: "Charities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Donation_Donators_DonatorId",
+                        column: x => x.DonatorId,
+                        principalTable: "Donators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cases_CharityId",
+                table: "Cases",
+                column: "CharityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Creditor_CaseID",
+                table: "Creditor",
+                column: "CaseID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donation_CharityId",
+                table: "Donation",
+                column: "CharityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donation_DonatorId",
+                table: "Donation",
+                column: "DonatorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "Creditor");
+
+            migrationBuilder.DropTable(
+                name: "Donation");
+
+            migrationBuilder.DropTable(
+                name: "Cases");
+
+            migrationBuilder.DropTable(
+                name: "Donators");
+
+            migrationBuilder.DropTable(
+                name: "Charities");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
